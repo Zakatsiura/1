4cc62c3f9cb6c881c8fe4533c4fa416fdafbdae7
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-09-14 15:04:55 +0300

    Sprintf

--- main.go
+++ main.go
@@ -24,17 +24,28 @@ func main() {
 	// ============== типы данных ===========
 
 	// var message float32
-	var message = []byte("asd")
-	var c rune = 'f'
+	// var message = []byte("asd")
+	// var c rune = 'f'
 
-	var b bool
+	// var b bool
 
 	// множественное присвоение:
-	d, e, f := 1, 2, 3
-	d, _, f = 3, 5, 7
-	e, f = f, e
-	d, e = 3, 6
+	// d, e, f := 1, 2, 3
+	// d, _, f = 3, 5, 7
+	// e, f = f, e
+	// d, e = 3, 6
 
 	// fmt.Println(reflect.TypeOf(message))
-	fmt.Println(message, b, c, d, e, f)
+	// fmt.Println(message, b, c, d, e, f)
+
+	message := sayHello("John", 45)
+	printMessage(message)
+}
+
+func printMessage(message string) {
+	fmt.Println(message)
+}
+
+func sayHello(name string, age int) string {
+	return fmt.Sprintf("Hello, %s! You are %d", name, age)
 }
